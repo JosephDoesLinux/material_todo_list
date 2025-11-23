8b899ed1521e07548dff9fd139bd41f4e25683c8
Joseph <kazumakiryu@tuta.io>
2025-11-24 01:56:36 +0200

    Home was STL, big mistake, fixed to STF

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'models/list.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
