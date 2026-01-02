0caebf67f3d2e050b75e262455f2a63f5f59cc75
Joseph <kazumakiryu@tuta.io>
2026-01-02 10:04:39 +0200

    php fix

--- php/addTask.php
+++ php/addTask.php
@@ -7,7 +7,7 @@ $details = $_POST['details'];
 $sql = "INSERT INTO tasks (title, details, is_completed) VALUES ('$title', '$details', 0)";
 
 if ($con->query($sql) === TRUE) {
-    echo json_encode(array("success" => true));
+    echo json_encode(array("success" => true, "id" => $con->insert_id));
 } else {
     echo json_encode(array("success" => false, "error" => $con->error));
 }
